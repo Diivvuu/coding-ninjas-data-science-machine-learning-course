84f743cde20219bedd7cca47910dbb3607ef371c
Diivvuu <95715357+Diivvuu@users.noreply.github.com>
2022-12-18 14:45:18 +0530

    web scraping

--- temp.cpp
+++ temp.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int input[], int size, int element) {
-int mid= size/2;
-// base case
-if (element > input[size - 1] || element < input[0] || (size == 1 && element != input[mid]) ) {
-  return -1;
-}
-if (element == input[mid]) {
-  return mid;
-} else if (element > input[mid]) {
-  int mid1 = binarySearch(input + mid, size - mid, element);
-  return mid + mid1;
-} else if (element < input[mid]) {
-  return binarySearch(input, mid, element);
-}
-}
 
-int main() {
-    int input[100000],length,element, ans;
-    cin >> length;
-    for(int i =0;i<length;i++)
-    { 
-        cin >> input[i];;
+#include <string.h>
+using namespace std;
+int getCodes(string input, string output[]) {
+    if(input[0] == '\0') {
+        output[0] = " ";
+        return 1;
     }
 
-    cin>>element;
-    ans = binarySearch(input, length, element);
-    cout<< ans << endl;
+    int first = input[0] - 48;
+    char firstChar = first + 'a' - 1;
+    char secondChar = '\0';
+    string smallOutput1[500];
+    string smallOutput2[500];
+    int size1 = getCodes(input.substr(1), smallOutput1);
+    int size2 = 0;
+    if(input[1] != '\0') {
+        int second = first*10 + input[1] - 48;
+        if(second >= 10 && second <= 26) {
+            secondChar = second + 'a' - 1;
+            size2 = getCodes(input.substr(2), smallOutput2);
+        }
+    }
+    int k = 0;
+    for(int i = 0; i < size1; i++ ){
+        output[k] = firstChar + smallOutput1[i];
+        k++;
+    }
+    for(int i = 0; i < size2; i++ ){
+        output[k] = secondChar + smallOutput2[i];
+        k++;
+    }   
+    return k; 
+}
+int main(){
+    string input;
+    cin >> input;
+
+    string output[10000];
+    int count = getCodes(input, output);
+    for(int i = 0; i < count && i < 10000; i++)
+        cout << output[i] << endl;
+    return 0;
 }
